4d77947da09544fadb48c52f5fffdcf8348ad4ba
MohammadAsheel <mohammadasheel1@gmail.com>
2025-09-19 15:22:57 +0530

    Initial commit - recipe API

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 
-// Import routes
-const studentRoutes = require('./routes/studentRoutes');
-app.use('/students', studentRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Student API is running!');
+// Example route
+app.get("/", (req, res) => {
+  res.send("API is running!");
 });
 
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
